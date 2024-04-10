17d043bb602709aca0092f29e3b1ec6014396a20
chadbanghart <cbanghar@gmail.com>
2024-04-10 15:11:31 -0700

    Lab part 2 complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,18 +2,53 @@ const Skill = require('../models/skill');
 
 module.exports = {
   index,
-  show
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update
 };
+
+function update(req, res) {
+  req.body.has = !!req.body.has;
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
+}
+
+function edit(req, res){
+  const skill = Skill.getOne(req.params.id);
+  res.render('skills/edit', {
+    title: 'Edit Skills',
+    skill
+  });
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect('/skills');
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill' });  
+}
 
 function index(req, res) {
   res.render('skills/index', {
-    skills: Skill.getAll()
+    skills: Skill.getAll(),
+    title: 'All Skills'
   });
 }
 
 function show(req, res) {
   // Do not use a leading slash when rendering
   res.render('skills/show', {
-    skill: Skill.getOne(req.params.id)
+    skill: Skill.getOne(req.params.id),
+    title: `Skills Details (#${req.params.id})`
   });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -7,7 +7,17 @@ const skillsCtrl = require('../controllers/skills');
 
 // GET /skills (index functionality/action --> list all skills) 
 router.get('/', skillsCtrl.index);
+// GET /skills/new <-- define BEFORE show route
+router.get('/new', skillsCtrl.new);
 // GET /skills/:id (show functionality/action --> display detail of a single skill)
 router.get('/:id', skillsCtrl.show);
+// GET /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// PUT /skills/:id
+router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
